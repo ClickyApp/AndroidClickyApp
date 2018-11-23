2a2e8a92b75018e4546b0f882f20ed866c65b233
Edward <doublyteam@gmail.com>
2018-11-23 15:05:57 -0500

    Initial Commit

---
+++ app/src/main/java/clicky/com/clickyapp/MainActivity.java
@@ -0,0 +1,13 @@
+package clicky.com.clickyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
